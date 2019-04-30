6ca368f8893b754ab7dd0a67058957adf155c053
santitee <santitee@gmail.com>
2019-04-30 08:09:16 +0700

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+
+import { ProductlistComponent } from './productlist/productlist.component';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'angular29-lab1';
+  @ViewChild('productList')
+ productList: ProductlistComponent;
+
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+
+import { ProductlistComponent } from './productlist/productlist.component';
+import { ProductitemComponent } from "./productitem/productitem.component";
+import { PanelComponent } from './panel/panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ProductlistComponent,
+    ProductitemComponent,
+    PanelComponent
   ],
   imports: [
     BrowserModule
